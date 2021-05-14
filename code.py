44fa2c53c59324732fde07e5a987ab75363094f2
AndreyOliveira2019 <55673289+AndreyOliveira2019@users.noreply.github.com>
2021-05-13 23:58:59 -0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,9 @@
+#crie sua matriz, aqui temos uma matriz de 2 linhas e 3 colunas:
+
+variavel = [[3,2,1],
+            [1,2,3]]
+
+#informe aqui as posições de linha e 
+#coluna que deseja trazer:
+
+variavel[0][0]
